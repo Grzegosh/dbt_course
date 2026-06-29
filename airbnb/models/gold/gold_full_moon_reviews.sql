d606da04535960b7c1f67fb67404562a5da5d1eb
Grzegosh <grzegorznaporowski1@gmail.com>
2026-06-29 22:14:52 +0200

    Sources and Seeds

---
+++ airbnb/models/gold/gold_full_moon_reviews.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        materialized = 'table',
+    )
+}}
+
+WITH fact_revews AS (
+
+    SELECT
+        *
+    FROM
+        {{ref('fact_reviews')}}
+
+), full_moon_reviews AS (
+
+    SELECT
+        *
+    FROM
+        {{ref('seed_full_moon_dates')}}
+)
+
+SELECT
+    r.*
+    ,CASE
+        WHEN fm.full_moon_date IS NULL THEN 'not full moon'
+        ELSE 'full moon'
+    END AS is_full_moon
+FROM
+    fact_reviews r 
+LEFT JOIN
+    full_moon_reviews fm
+ON
+    TO_DATE(r.review_date) = DATEADD(DAY, 1, fm.full_moon_date)
